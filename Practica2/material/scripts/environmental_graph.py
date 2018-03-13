7401b6a248711c09ddfb8e4a448341cc3b5a4ce9
Francisco Luque <fluque1995@gmail.com>
2018-03-13 21:47:01 +0100

    Comenzada última parte del caso práctico

--- Practica2/material/scripts/environmental_graph.py
+++ Practica2/material/scripts/environmental_graph.py
@@ -5,6 +5,8 @@ import pandas as pd
 data = pd.read_csv('../environmental.csv', index_col='Concept')
 data = data[data.columns[::-1]] 
 
+print(data)
+
 trace1 = go.Scatter(
     x=data.columns,
     y=data.iloc[0].tolist(),
@@ -14,54 +16,17 @@ trace2 = go.Scatter(
     x=data.columns,
     y=data.iloc[1].tolist(),
     name='Uso de agua',
-    yaxis='y2'
 )
 trace3 = go.Scatter(
     x=data.columns,
     y=data.iloc[2].tolist(),
     name='Emisiones de CO2',
-    yaxis='y3'
 )
 
 layout = go.Layout(
-    title='Environmental summary',
-    width=800,
+    title='Gasto medioambiental',
     xaxis=dict(
         domain=data.columns
-    ),
-    yaxis=dict(
-        title='yaxis title',
-        titlefont=dict(
-            color='#1f77b4'
-        ),
-        tickfont=dict(
-            color='#1f77b4'
-        )
-    ),
-    yaxis2=dict(
-        title='yaxis2 title',
-        titlefont=dict(
-            color='#ff7f0e'
-        ),
-        tickfont=dict(
-            color='#ff7f0e'
-        ),
-        anchor='free',
-        overlaying='y',
-        side='left',
-        position=0.15
-    ),
-    yaxis3=dict(
-        title='yaxis4 title',
-        titlefont=dict(
-            color='#d62728'
-        ),
-        tickfont=dict(
-            color='#d62728'
-        ),
-        anchor='x',
-        overlaying='y',
-        side='right'
     ),
 )
 data=[trace1, trace2, trace3]
